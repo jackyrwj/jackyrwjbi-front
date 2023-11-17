cde736115fefd48b41c8306abe384289a4a7c08f
jackyrwj <763214954@qq.com>
2023-11-17 12:14:58 +0800

    ooo'

--- src/pages/User/Login/index.tsx
+++ src/pages/User/Login/index.tsx
@@ -176,7 +176,6 @@ const Login: React.FC = () => {
           logo={<img alt="logo" src="/logo_old.svg" />}
           title="Jackyrwj BI"
           subTitle={intl.formatMessage({ id: 'pages.layouts.userLayout.title' })}
-          {/*"test"*/}
           initialValues={{
             autoLogin: true,
           }}
